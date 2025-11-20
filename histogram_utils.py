cbdd58e5d25623d138e937448f12dc4df956bec2
DuvManR <roeiduv@gmail.com>
2025-11-20 11:27:41 +0200

    Update histogram_utils.py

--- histogram_utils.py
+++ histogram_utils.py
@@ -1,4 +1,4 @@
-import matplotlib.pyploy as plt
+from matplotlib import pyploy as plt
 
 def build_histogram(data):
     my_dict = {}
@@ -10,7 +10,6 @@ def build_histogram(data):
     return my_dict
 
 def plot_histogram(histogram):
-    global plt
     x_values = list(histogram.keys())
     y_values = list(histogram.values())
 
